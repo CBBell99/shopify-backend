554bad3f9e5b4b28f21cbbcf25cb96604e016b9e
CBBell99 <chrisbeckertonbell@gmail.com>
2022-05-10 17:48:44 -0700

    create db reset, basic server created

--- server.js
+++ server.js
@@ -1,1 +1,18 @@
+// load .env data into process.env
+require('dotenv').config();
+
+// Web server config
+const PORT = process.env.PORT || 3000;
 const express = require("express");
+const app = express();
+const morgan = require("morgan")
+
+app.use(morgan("dev"))
+
+app.get('/', (req, res) =>
+  res.send("hello")
+)
+
+app.listen(PORT, () => {
+  console.log(`Server listening on port ${PORT}`)
+})
