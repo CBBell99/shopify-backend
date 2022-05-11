70f674eff46b6c8894b21710835eb70ba89ea706
CBBell99 <chrisbeckertonbell@gmail.com>
2022-05-11 16:16:13 -0700

    add schema & seeds

---
+++ db/seeds/02_products.sql
@@ -0,0 +1,4 @@
+INSERT INTO products (product_name, quantity) 
+VALUES ('iPad', 10);
+VALUES ('MacBook Pro', 5);
+VALUES ('AirPods', 1);
---
+++ db/seeds/03_shipments.sql
@@ -0,0 +1,1 @@
+INSERT INTO shipments(user_id, product_id) VALUES (1, 1);
