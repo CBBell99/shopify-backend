4852bbc9346f9cde29e57f29179c39fd4ef69382
CBBell99 <chrisbeckertonbell@gmail.com>
2022-05-13 21:46:57 -0700

    shipments post route

--- routes/shipments.js
+++ routes/shipments.js
@@ -2,16 +2,27 @@ const express = require("express");
 const router = express.Router();
 
 module.exports = (db) => {
-  router.get("/", (req, res) => {
-    db.query(`SELECT * FROM shipments;`)
-      .then((data) => {
-        console.log(data.rows);
+  router
+    .get("/", (req, res) => {
+      db.query(`SELECT * FROM shipments;`)
+        .then((data) => {
+          console.log(data.rows);
+          const shipments = data.rows;
+          res.json({ shipments });
+        })
+        .catch((err) => {
+          res.status(500).json({ error: err.message });
+        });
+    })
+    .post("/", (req, res) => {
+      db.query(
+        `INSERT INTO shipments(user_id, product_id)
+        VALUES ($1, $2) RETURNING *`,
+        [req.body.user_id, req.body.product_id]
+      ).then((data) => {
         const shipments = data.rows;
         res.json({ shipments });
-      })
-      .catch((err) => {
-        res.status(500).json({ error: err.message });
       });
-  });
+    });
   return router;
 };
